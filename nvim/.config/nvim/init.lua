8ff57c37f1712dde1a31fce3417b17471b597f1a
LaBatata101 <labatata101@linuxmail.org>
2022-02-01 19:36:03 -0300

    move plugin config loading to packer.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,24 +1,22 @@
+require("config.globals")
 require("config.settings")
 require("config.keymaps")
+
+pcall(require, "impatient")
 
 require("config.plugins.packer")
 
 -- Improve startup speed
-require("impatient")
+-- require("impatient")
 
 require("packer_compiled")
 -- load plugins configurations
-require("config.plugins.treesitter")
-require("config.plugins.lsp")
 require("config.plugins.cmp")
 require("config.plugins.hop")
 require("config.plugins.comment")
 require("config.plugins.lualine")
 require("config.plugins.autopairs")
-require("config.plugins.lightbulb")
 require("config.plugins.neoscroll")
 require("config.plugins.bufferline")
 require("config.plugins.illuminate")
 require("config.plugins.tokyonight")
-require("config.plugins.lsp_signature")
-require("config.plugins.indent_blankline")
