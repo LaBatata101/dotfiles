d30f4ce39bd09739e1c423ad278b62fe4fb78648
LaBatata101 <labatata101@linuxmail.org>
2022-02-01 19:30:44 -0300

    disable Illuminate in these filetypes

--- nvim/.config/nvim/lua/config/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/config/plugins/illuminate.lua
@@ -1,1 +1,8 @@
-vim.g.Illuminate_ftblacklist = {"help"}
+vim.g.Illuminate_ftblacklist = {
+  "help",
+  "lsp-installer",
+  "dapui_scopes",
+  "dapui_stacks",
+  "dapui_watches",
+  "dapui_breakpoints",
+}
