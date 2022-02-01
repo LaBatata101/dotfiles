deea37b39c625efd3aadb5db426e6954117c8f1b
LaBatata101 <labatata101@linuxmail.org>
2022-02-01 19:32:11 -0300

    add select-ui extension

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,15 +1,33 @@
-require('telescope').setup {
-    defaults = {
-        prompt_prefix = '🔍 ',
-        selection_caret = " ",
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
+
+telescope.setup {
+  defaults = {
+    -- winblend = 30,
+    prompt_prefix = '🔍 ',
+    selection_caret = " ",
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<M-p>"] = action_layout.toggle_preview
+      },
     },
-    pickers = {
-        lsp_code_actions = {
-            theme = "cursor",
-        },
-        find_files = {
-            theme = "dropdown",
-            previewer = false,
-        }
+  },
+  pickers = {
+    lsp_code_actions = {
+      theme = "cursor",
+    },
+    find_files = {
+      theme = "dropdown",
+      previewer = false,
     }
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_cursor()
+    }
+  }
 }
+
+telescope.load_extension("ui-select")
