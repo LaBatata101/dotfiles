69ba48a5efadb98db853b2e5fce7b4b45f429ce6
LaBatata101 <labatata101@linuxmail.org>
2022-02-01 19:36:48 -0300

    customize indent blankline

--- nvim/.config/nvim/lua/config/plugins/indent_blankline.lua
+++ nvim/.config/nvim/lua/config/plugins/indent_blankline.lua
@@ -1,3 +1,16 @@
 require("indent_blankline").setup {
-    char = "┆",
+  char_list = {'|', '¦', '┆', '┊'},
+  filetype_exclude = {
+    "text",
+    "help",
+    "lsp-installer",
+    "dapui_scopes",
+    "dapui_stacks",
+    "dapui_watches",
+    "dapui_breakpoints",
+  },
+  show_first_indent_level = false,
+  show_trailing_blankline_indent = false,
+  show_current_context = true,
+  show_current_context_start = false,
 }
