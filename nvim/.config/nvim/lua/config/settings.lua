e48e044c232d5a14927678d813db758a81e0091f
LaBatata101 <labatata101@linuxmail.org>
2022-03-02 17:26:35 -0300

    formating with stylua and add autocommand for terminal buffer

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -1,12 +1,12 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.g.python3_host_prog = '/usr/bin/python'
+vim.g.python3_host_prog = "/usr/bin/python"
 vim.g.loaded_python_provider = 0
 vim.g.python_host_skip_check = 1
 
 vim.g.cursorhold_updatetime = 250
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.cursorline = true
 -- vim.opt.nohlsearch = true
 vim.opt.showmode = false
@@ -15,12 +15,12 @@ vim.opt.hidden = true -- allow buffer switching without saving
 
 vim.opt.showtabline = 2 -- always show tabline
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.number = true
 vim.opt.rnu = true
 
-vim.opt.colorcolumn = '120'
+vim.opt.colorcolumn = "120"
 
 vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 5
@@ -31,15 +31,15 @@ vim.opt.shiftwidth = 4
 
 vim.opt.undolevels = 1000
 
-vim.opt.wildignore:append {'*.o', '*.pyc', '.git/', '__pycache__/'}
+vim.opt.wildignore:append({ "*.o", "*.pyc", ".git/", "__pycache__/" })
 
 vim.opt.termguicolors = true
 
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 vim.opt.autoindent = true
 
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 vim.opt.pumblend = 20
 
@@ -51,11 +51,20 @@ vim.opt.linebreak = true
 vim.opt.emoji = false
 
 -- folding config
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldmethod = "expr"
 vim.opt.foldlevelstart = 99
 
-vim.cmd [[
+-- Splitting a new window below the current one
+vim.opt.splitbelow = true
+-- Splitting a new window at the right of the current one
+vim.opt.splitright = true
+
+-- Disable continuation of comments to the next linebreak
+vim.opt.formatoptions:remove({ "c" })
+-- vim.o.formatoptions = "cql"
+
+vim.cmd([[
   augroup VimrcIncSearchHighlight
     autocmd!
     autocmd CmdlineEnter [/\\?] :set hlsearch
@@ -67,7 +76,7 @@ vim.cmd [[
     autocmd CmdlineEnter [:s\\:%s] :set hlsearch
     autocmd CmdlineLeave [:s\\:%s] :set nohlsearch
   augroup END
-]]
+]])
 
 -- disable builtin plugins
 vim.g.loaded_gzip = 1
@@ -90,3 +99,12 @@ vim.g.loaded_rrhelper = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
+
+vim.cmd([[
+  augroup noevim_terminal
+    autocmd!
+      autocmd TermOpen * startinsert
+      " autocmd TermOpen * setlocal ft=terminal-hide-lualine
+      autocmd TermOpen * setlocal nonumber norelativenumber
+  augroup END
+]])
