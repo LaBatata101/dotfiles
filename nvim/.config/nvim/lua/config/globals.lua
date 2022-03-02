a61d9f26292981b00d67965558be16a2f91f0609
LaBatata101 <labatata101@linuxmail.org>
2022-03-02 17:25:13 -0300

    add globals variables

--- nvim/.config/nvim/lua/config/globals.lua
+++ nvim/.config/nvim/lua/config/globals.lua
@@ -1,1 +1,11 @@
-vim.g.supported_languages = {"rust", "python", "lua", "c", "vim"}
+vim.g.supported_languages = { "rust", "python", "lua", "c", "vim" }
+
+vim.g.treesitter_parsers = {}
+table.insert(vim.g.treesitter_parsers, vim.g.supported_languages)
+table.insert(vim.g.treesitter_parsers, "comment")
+
+vim.g.language_servers = {
+  "pyright",
+  "rust-analyzer",
+  "sumneko_lua",
+}
